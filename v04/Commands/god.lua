64e7ab835821e702b79771dc26027ac44d7597c7
EngiAdurite <49373598+greasemonkey123@users.noreply.github.com>
2021-01-08 12:22:56 +0200

    Create god.lua

---
+++ v04/Commands/god.lua
@@ -0,0 +1,41 @@
+OnFire = function(plr,arg,env)
+	local targets = env.HandlePlayers(plr.Name,arg[2],2,false)
+	if targets[1] == nil then
+		return {false, env.CurrentLanguage.CommandExec.NoFound}
+	elseif targets[1] == false then
+		return {false, targets[2]}
+	end
+	local done = {0,""}
+	for _,v in pairs(targets) do
+		v.Character.Humanoid.MaxHealth = math.huge
+		v.Character.Humanoid.Health = v.Character.Humanoid.MaxHealth
+		if done[2] ~= "" then
+			done[2] = done[2]..", "..v.Name
+			done[1] = done[1]+1
+		else
+			done[2] = v.Name
+			done[1] = 1
+		end
+	end
+	if done[1] >= 2 then
+		return {true,"Successfully turned "..done[1].." people into gods."}
+	else
+		return {true,"Successfully turned "..done[2].." into a god."}
+	end
+end
+
+OnLoad = function(env)
+end
+
+return {
+	ModuleType = "Command",
+	Usage = "<players>", 
+	ModName = "god",
+	Alias = {},
+	Level = 3,
+	Sandbox = true,
+	Libraries = {},
+	Dependencies = {}, -- To be used soon.
+	ModFunction = OnFire,
+	ModLoad = OnLoad,
+}
